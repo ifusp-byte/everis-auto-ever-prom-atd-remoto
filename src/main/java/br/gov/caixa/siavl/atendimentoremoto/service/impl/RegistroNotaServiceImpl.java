b7639d1b90e157b8b52c5c2ae7efecda970bed50
Mariana de Amorim Delfino <f737554@corp.caixa.gov.br>
2024-04-03 10:10:31 -0300

    adicionada estrutura gravacao nota

--- src/main/java/br/gov/caixa/siavl/atendimentoremoto/service/impl/RegistroNotaServiceImpl.java
+++ src/main/java/br/gov/caixa/siavl/atendimentoremoto/service/impl/RegistroNotaServiceImpl.java
@@ -2,9 +2,17 @@ package br.gov.caixa.siavl.atendimentoremoto.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import br.gov.caixa.siavl.atendimentoremoto.dto.RegistraNotaInputDto;
 import br.gov.caixa.siavl.atendimentoremoto.service.RegistroNotaService;
 
 @Service
 public class RegistroNotaServiceImpl implements RegistroNotaService {
+
+	@Override
+	public Object registraNota(String token, Long numeroNota, RegistraNotaInputDto registraNotaInputDto)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
