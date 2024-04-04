124da7e8d3f8c4da77adecfead69fda07c973fad
Mariana de Amorim Delfino <f737554@corp.caixa.gov.br>
2024-04-04 12:00:53 -0300

    adicionada parametrizacao roteiro nota

---
+++ src/main/java/br/gov/caixa/siavl/atendimentoremoto/dto/RegistraNotaOutputDto.java
@@ -0,0 +1,27 @@
+package br.gov.caixa.siavl.atendimentoremoto.dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "RegistraNotaOutputDto")
+public class RegistraNotaOutputDto {
+	
+	private Boolean statusNotaRegistrada;
+	private String mensagem;
+
+}
--- src/main/java/br/gov/caixa/siavl/atendimentoremoto/service/impl/RegistroNotaServiceImpl.java
+++ src/main/java/br/gov/caixa/siavl/atendimentoremoto/service/impl/RegistroNotaServiceImpl.java
@@ -1,18 +1,60 @@
 package br.gov.caixa.siavl.atendimentoremoto.service.impl;
 
+import java.sql.Clob;
+import javax.sql.rowset.serial.SerialClob;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import br.gov.caixa.siavl.atendimentoremoto.dto.RegistraNotaInputDto;
+import br.gov.caixa.siavl.atendimentoremoto.dto.RegistraNotaOutputDto;
+import br.gov.caixa.siavl.atendimentoremoto.model.RelatorioNotaNegociacao;
+import br.gov.caixa.siavl.atendimentoremoto.repository.EquipeAtendimentoRepository;
+import br.gov.caixa.siavl.atendimentoremoto.repository.NotaNegociacaoRepository;
+import br.gov.caixa.siavl.atendimentoremoto.repository.RelatorioNotaNegociacaoRepository;
 import br.gov.caixa.siavl.atendimentoremoto.service.RegistroNotaService;
+import br.gov.caixa.siavl.atendimentoremoto.util.TokenUtils;
 
 @Service
 public class RegistroNotaServiceImpl implements RegistroNotaService {
 
+	@Autowired
+	RelatorioNotaNegociacaoRepository relatorioNotaNegociacaoRepository;
+
+	@Autowired
+	EquipeAtendimentoRepository equipeAtendimentoRepository;
+
+	@Autowired
+	NotaNegociacaoRepository notaNegociacaoRepository;
+
+	@Autowired
+	TokenUtils tokenUtils;
+
 	@Override
 	public Object registraNota(String token, Long numeroNota, RegistraNotaInputDto registraNotaInputDto)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		Long numeroUnidade = Long.parseLong(tokenUtils.getUnidadeFromToken(token));
+		Long numeroEquipe = null;
+
+		numeroEquipe = equipeAtendimentoRepository.findEquipeByUnidade(numeroUnidade);
+
+		RegistraNotaOutputDto registraNotaOutputDto = new RegistraNotaOutputDto();
+
+		if (numeroEquipe == null) {
+
+			registraNotaOutputDto = RegistraNotaOutputDto.builder().statusNotaRegistrada(false)
+					.mensagem("A unidade não possui equipe vinculada.").build();
+
+		} else {
+			Clob relatorioNota = new SerialClob(registraNotaInputDto.getRelatorioNota().toCharArray());
+			RelatorioNotaNegociacao relatorioNotaNegociacao = new RelatorioNotaNegociacao(); 
+			relatorioNotaNegociacao.setNumeroEquipe(numeroEquipe);
+			relatorioNotaNegociacao.setRelatorioNota(relatorioNota);		
+			relatorioNotaNegociacaoRepository.save(relatorioNotaNegociacao);		
+			registraNotaOutputDto = RegistraNotaOutputDto.builder().statusNotaRegistrada(true)
+					.mensagem("Nota registrada com sucesso!").build();
+		}
+
+		return registraNotaOutputDto;
 	}
 
 }
