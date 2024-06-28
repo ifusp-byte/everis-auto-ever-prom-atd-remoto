eff660eea79121ff6e4d9c76f058546dbf0e522d
Mariana de Amorim Delfino <f737554@corp.caixa.gov.br>
2024-06-28 13:20:56 -0300

    adicionda busca por equipe sr

--- src/main/java/br/gov/caixa/siavl/atendimentoremoto/repository/EquipeAtendimentoRepository.java
+++ src/main/java/br/gov/caixa/siavl/atendimentoremoto/repository/EquipeAtendimentoRepository.java
@@ -11,5 +11,8 @@ public interface EquipeAtendimentoRepository extends JpaRepository<EquipeAtendim
 	@Query("SELECT " + "A.numeroEquipe " + "FROM " + "EquipeAtendimento A " 
 			+ "WHERE " + "A.equipeAtiva = 1 " + "AND " + "A.numeroUnidadeEquipe = ?1")
 	Long findEquipeByUnidade(Long unidade);
+	
+	@Query(value = "SELECT B.NU_EQUIPE_ATENDIMENTO FROM AVL.AVLTB014_UNIDADE_DIGITAL_ATNTO A, AVL.AVLTB009_EQUIPE_ATENDIMENTO B WHERE A.NU_UNIDADE_DIGITAL_ATNTO_VRTL = B.NU_UNIDADE_EQUIPE AND B.IC_EQUIPE_ATIVA = 1 AND ROWNUM = 1 AND A.NU_UNIDADE_SR = ?1", nativeQuery = true)
+	Long findEquipeByUnidadeSR(Long unidade);
 
 }
