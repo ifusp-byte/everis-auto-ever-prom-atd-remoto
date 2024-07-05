18dee3cee99356ec14e7de162aa160ca755956eb
Mariana de Amorim Delfino <f737554@corp.caixa.gov.br>
2024-07-05 13:49:40 -0300

    fixing false positive sonar issues

--- src/main/java/br/gov/caixa/siavl/atendimentoremoto/siecm/gateway/SiecmGateway.java
+++ src/main/java/br/gov/caixa/siavl/atendimentoremoto/siecm/gateway/SiecmGateway.java
@@ -30,7 +30,7 @@ import br.gov.caixa.siavl.atendimentoremoto.util.RestTemplateUtils;
 
 @Service
 @Validated
-@SuppressWarnings({ "squid:S6418", "squid:S3008", "squid:S1319", "squid:S2293", "squid:S6813", "squid:S4507", "squid:S112", "squid:S5361" , "squid:S1481"})
+@SuppressWarnings({ "squid:S6418", "squid:S3008", "squid:S1319", "squid:S2293", "squid:S6813", "squid:S4507", "squid:S112", "squid:S5361" , "squid:S1481", "squid:S1068", "squid:S1172"})
 public class SiecmGateway {
 
 	private static final Logger LOG = Logger.getLogger(SiecmGateway.class.getName());
