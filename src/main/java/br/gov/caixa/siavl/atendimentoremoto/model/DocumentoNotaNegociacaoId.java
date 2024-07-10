a5e0ed4008513f1002a5051d568d3a1a21a3b937
Mariana de Amorim Delfino <f737554@corp.caixa.gov.br>
2024-07-10 16:36:40 -0300

    chave primaria composta

---
+++ src/main/java/br/gov/caixa/siavl/atendimentoremoto/model/DocumentoNotaNegociacaoId.java
@@ -0,0 +1,29 @@
+package br.gov.caixa.siavl.atendimentoremoto.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class DocumentoNotaNegociacaoId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String tipoPessoa;
+	private Long numeroNota;
+	private Long cpfCnpjCliente;
+	private Long tipoDocumentoCliente;
+	private Date inclusaoDocumento;
+	public DocumentoNotaNegociacaoId(String tipoPessoa, Long numeroNota, Long cpfCnpjCliente, Long tipoDocumentoCliente,
+			Date inclusaoDocumento) {
+		this.tipoPessoa = tipoPessoa;
+		this.numeroNota = numeroNota;
+		this.cpfCnpjCliente = cpfCnpjCliente;
+		this.tipoDocumentoCliente = tipoDocumentoCliente;
+		this.inclusaoDocumento = inclusaoDocumento;
+	}
+	
+	
+
+}
