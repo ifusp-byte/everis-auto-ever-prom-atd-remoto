4a57ae746c5874b0a86c03f841c2ac981d0b09ca
Mariana de Amorim Delfino <f737554@corp.caixa.gov.br>
2025-01-16 21:36:08 -0300

    teste trava esteira

--- src/test/java/br/gov/caixa/siavl/atendimentoremoto/controller/modelonota/AtendimentoRemotoControllerModeloNotaTestesTest.java
+++ src/test/java/br/gov/caixa/siavl/atendimentoremoto/controller/modelonota/AtendimentoRemotoControllerModeloNotaTestesTest.java
@@ -31,7 +31,7 @@ class AtendimentoRemotoControllerModeloNotaTestesTest extends AtendimentoRemotoC
 		consultaModeloNotaMaisUtilizada();
 	}
 
-	//@Test
+	@Test //teste 
 	void consultaModeloNotaFavoritaTest() {
 		consultaModeloNotaFavorita();
 	}
