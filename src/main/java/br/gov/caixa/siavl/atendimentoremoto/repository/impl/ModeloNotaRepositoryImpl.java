972c44dd5aa9628d134453bbcaf3db246ae025ae
Mariana de Amorim Delfino <f737554@corp.caixa.gov.br>
2025-03-27 08:07:21 -0300

    ajuste

--- src/main/java/br/gov/caixa/siavl/atendimentoremoto/repository/impl/ModeloNotaRepositoryImpl.java
+++ src/main/java/br/gov/caixa/siavl/atendimentoremoto/repository/impl/ModeloNotaRepositoryImpl.java
@@ -1,5 +1,7 @@
 package br.gov.caixa.siavl.atendimentoremoto.repository.impl;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import jakarta.persistence.EntityManager;
@@ -13,6 +15,9 @@ public class ModeloNotaRepositoryImpl {
 	private EntityManager em;
 
 	public List<Object[]> modeloNotaMaisUtilizada(Long publicoAlvo) {
+		
+		LocalDate localDate = LocalDate.now().minusDays(365);
+		Date date = java.sql.Date.valueOf(localDate);
 
 		StringBuilder sb = new StringBuilder();
 
@@ -20,11 +25,11 @@ public class ModeloNotaRepositoryImpl {
 		sb.append(" FROM  NotaNegociacao A, ModeloNotaNegocio B, AcaoProduto C, TipoNota D ");
 		sb.append(" WHERE D.numeroTipoNota = B.numeroTipoNota AND A.numeroModeloNota = B.numeroModeloNota ");
 		sb.append(" AND B.numeroAcao = C.numeroAcao ");
-		sb.append(" AND A.dataCriacaoNota > CURRENT_DATE - INTERVAL '365' DAY ");
+		sb.append(" AND A.dataCriacaoNota > :dataCriacao  ");
 		sb.append(" AND B.modeloNotaAtivo = 1 ");
 		sb.append(" AND B.contratacaoAutomatica = 'N' ");
 		sb.append(" AND B.situacaoModeloNota = 'P' ");
-		sb.append(" AND C.acaoProdutoAtivo = 1 ");
+		sb.append(" AND C.acaoProdutoAtivo = 1 ");	
 		sb.append(" AND B.numeroModeloNota NOT IN (SELECT C.numeroModeloNota FROM FluxoAtendimento C) ");
 		sb.append(" AND B.publicoAlvo <> :publicoAlvo ");
 		sb.append(" GROUP  BY A.numeroModeloNota, C.numeroAcao, C.descricao, D.numeroTipoNota, D.nomeTipoNota ");
@@ -32,6 +37,7 @@ public class ModeloNotaRepositoryImpl {
 
 		TypedQuery<Object[]> query = em.createQuery(sb.toString(), Object[].class);
 		query.setParameter("publicoAlvo", publicoAlvo);
+		query.setParameter("dataCriacao", date);
 		query.setMaxResults(5);
 
 		return query.getResultList();
