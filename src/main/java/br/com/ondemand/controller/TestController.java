b08415b5bbe1942d11a5bbaea05ba4970e4f3533
Felipe Gewers <ifusp@marianadelfino.de>
2025-11-08 14:25:40 -0300

    Delete src/main directory

--- src/main/java/br/com/ondemand/controller/TestController.java
+++
@@ -1,9 +0,0 @@
-package br.com.ondemand.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = { "/", "/test" })
-public class TestController extends Liveness {
-}
